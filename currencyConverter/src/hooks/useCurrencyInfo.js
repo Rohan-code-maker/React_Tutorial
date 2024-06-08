aed61db7bb01a918233386008851f65a772042ff
Rohan Kumar Gupta <116427952+Rohan-code-maker@users.noreply.github.com>
2024-06-08 15:37:01 +0530

    Currency Converter Project

---
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
+            .then((res) => res.json())
+            .then((res) => {
+                setData(res.rates);
+                console.log(res.rates); // Log the fetched data
+            })
+            .catch((error) => {
+                console.error("Error fetching currency data:", error); // Proper error handling
+            });
+    }, [currency]);
+
+    return data;
+}
+
+export default useCurrencyInfo;
