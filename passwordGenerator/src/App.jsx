0627bd2c2def8d4d585531d49d4fc81b0e9b2acf
Rohan Kumar Gupta <rohan.gupta2059@gmail.com>
2025-07-26 23:43:44 +0530

    Password generator practice

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,105 +1,71 @@
 import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
-  const [length, setlength] = useState(8);
-  const [number, setNumber] = useState(false);
-  const [char, setChar] = useState(false);
+  const [length, setLength] = useState(8);
+  const [isNumber, setisNumber] = useState(false);
+  const [isChar, setisChar] = useState(false);
   const [password, setPassword] = useState("");
 
-  //useCallback is to memoize the function i.e to store it in memory
+  //useCallback is for memoization(remember previous values)
   const passwordGenerator = useCallback(() => {
-    let pass = "";
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
-    if (number) {
-      str += "0123456789";
-    }
-    if (char) {
-      str += "!@#$%^&*{}[]+-=_`~";
-    }
-
+    let pass = ""
+    let str = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"
+    if (isNumber) str += "0123456789"
+    if (isChar) str += ")(*&^%$#@!{}[]~`:?"
     for (let i = 1; i <= length; i++) {
-      let char = Math.floor(Math.random() * str.length + 1);
-      pass += str.charAt(char);
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
     }
 
-    setPassword(pass);
-  }, [length, number, char, setPassword]);
-
-  //useEffect is used to run the function when the project starts
-  useEffect(() => {
-    passwordGenerator();
-  }, [length, number, char, passwordGenerator]);
-
-  //useRef is used to give refernece from a tag to another
-  const passwordRef = useRef(null);
+    setPassword(pass)
+  }, [length, isChar, isNumber, setPassword])
 
   const copyPassword = useCallback(() => {
-    passwordRef.current?.select(); //for selecting the password
-    passwordRef.current?.setSelectionRange(0, 3); //for selecting the range of the password
-    window.navigator.clipboard.writeText(password); //for copying the password to clipboard
-  }, [password]);
+    passwordRef.current?.select()
+    // passwordRef.current?.setSelectionRange(0,3)
+    window.navigator.clipboard.writeText(password)
+  },[password])
+
+  //useEffect is called when the browser starts
+  useEffect(()=> {
+    passwordGenerator()
+  },[length, isChar, isNumber, passwordGenerator])
+
+  //useRef reference lena hai kisika  tab use karte hai
+  const passwordRef = useRef(null)
 
   return (
     <>
-      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800">
-        <h1 className="text-white text-center my-3">Password Generator</h1>
-        <div className="flex shadow rounded-lg overflow-hidden mb-4">
-          <input
-            type="text"
-            value={password}
-            className="outline-none w-full py-1 px-3"
-            placeholder="Password"
-            readOnly
-            ref={passwordRef}
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700">
+        <div>
+          <input type="text" value={password} placeholder="password" readOnly
+          ref={passwordRef}
           />
-          <button
-            onClick={copyPassword}
-            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
-          >
-            Copy
-          </button>
+          <button onClick={copyPassword}>copy</button>
         </div>
-        <div className="flex text-sm gap-x-2">
-          <div className="flex items-center gap-x-1">
+        <div>
+          <div>
             <input
-              type="range"
-              min={6}
-              max={100}
-              value={length}
-              className="cursor-pointer"
-              onChange={(e) => {
-                setlength(e.target.value);
-              }}
-            />
-            <label>Length: {length}</label>
+            onChange={(e) => {setLength(e.target.value)}}
+             type="range" min={6} max={100} value={{length}} className="cursor-pointer" />
+            <label htmlFor="">Length:{length}</label>
           </div>
-          <div className="flex items-center gap-x-1">
-            <input
-              type="checkbox"
-              defaultChecked={number}
-              id="numberInput"
-              onChange={() => {
-                setNumber((prev) => !prev);
-              }}
-            />
-            <label htmlFor="numberInput">Number</label>
+          <div>
+            <input type="checkbox" defaultChecked={isNumber} id="numberInput" onChange={() => {
+              setisNumber((prev) => !prev)
+            }} />
+            <label htmlFor="">Numbers</label>
           </div>
-          <div className="flex items-center gap-x-1">
-            <input
-              type="checkbox"
-              defaultChecked={char}
-              id="charInput"
-              onChange={() => {
-                setChar((prev) => !prev);
-              }}
-            />
-            <label htmlFor="characterInput">Characters</label>
+          <div>
+            <input type="checkbox" defaultChecked={isChar} id="charInput" onChange={() => {
+              setisChar((prev) => !prev)
+            }} />
+            <label htmlFor="">Char</label>
           </div>
         </div>
       </div>
     </>
-  );
+  )
 }
 
 export default App;
